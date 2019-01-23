56e2d7209a6faf47675420e16aacb8019708f162
gbock72 <35757970+gbock72@users.noreply.github.com>
2019-01-23 08:06:31 +0100

    Update install_homebrew.rb

--- install_homebrew.rb
+++ install_homebrew.rb
@@ -122,7 +122,8 @@ begin
     # pipefail to cause the exit status from curl to propogate if it fails
     # REMOVED pipefail option (not available on Tiger PPC).
     # REMOVED SSL security, using curl option '-k' (certificate can't be checked due to too old curl-ca.cert).
-    system "bash -c 'curl -vfLk https://github.com/gbock72/tigerbrew/tree/patch-1 | tar -xv'"
+    #system "bash -c 'curl -vsSfLk https://github.com/gbock72/tigerbrew/tree/patch-1 | tar -xzv'"
+    system "bash -c 'wget -c https://github.com/gbock72/tigerbrew/tree/patch-1/tigerbrew && tar -zcf  tigerbrew.tar.gz /tigerbrew'"
   end
 ensure
   # we reset the permissions of /usr/local because we want to minimise the
